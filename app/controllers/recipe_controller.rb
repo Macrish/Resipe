6c84977b7f1bdfdbb6446e901f6e38f8d669604f
Tatiana Davydiuk <tatianadavydiuk@gmail.com>
2017-09-09 13:54:03 +0300

    finish page resipe

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,5 +1,18 @@
 class RecipeController < ApplicationController
-  def dish
 
+  def dish
+    @flour = 3
+    @eggs = 2
+    @sugar = 5
+    @tvorog = 500
+
+    if params[:people]
+      koef = params[:people].to_i
+      @flour *= koef
+      @eggs *= koef
+      @sugar *= koef
+      @tvorog *= koef
+    end
   end
+
 end
